12a911f9ef74f6f52d72bd7d6c1ba98a2049bfd6
Ihor Tonkovyd <142664318+ITonkovyd@users.noreply.github.com>
2023-08-19 16:16:45 +0300

    add dependency

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,35 +1,27 @@
 import { useEffect } from 'react';
-import { createPortal } from "react-dom";
-import { ModalStyle, Overlay } from "./Modal.styled";
+import { createPortal } from 'react-dom';
+import { ModalStyle, Overlay } from './Modal.styled';
 
-const modalRoot = document.querySelector('#modal-root')
-
-
+const modalRoot = document.querySelector('#modal-root');
 
 export default function Modal({ children, onClose }) {
-  
   useEffect(() => {
-    
     function handleKeyDown(e) {
       if (e.code === 'Escape') {
-        onClose()
+        onClose();
       }
     }
-    window.addEventListener('keydown', handleKeyDown)
-    
-    return  () => {
-      window.removeEventListener('keydown', handleKeyDown)
-    }
-    
-  }, []);
-  
-  
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [onClose]);
+
   return createPortal(
     <Overlay>
-      <ModalStyle>
-        {children}
-      </ModalStyle>
+      <ModalStyle>{children}</ModalStyle>
     </Overlay>,
     modalRoot
-  )
+  );
 }
