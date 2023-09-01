037c60af30ac4af2ca3f6b3aa5277f7e1f435b82
Ihor Tonkovyd <142664318+ITonkovyd@users.noreply.github.com>
2023-09-01 17:44:52 +0300

    fixing dependence in useEffect hook

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -4,9 +4,8 @@ import ImageGalleryItem from 'components/ImageGalleryItem';
 import Loader from 'components/Loader';
 import PropTypes from 'prop-types';
 import { useEffect, useState } from 'react';
-import { toast, ToastContainer } from 'react-toastify';
+import { ToastContainer, toast } from 'react-toastify';
 import { ImageGalleryStyle, Wrapper } from './ImageGallery.styled';
-
 
 export default function ImageGallery({ search }) {
   const [prevSearch, setPrevSearch] = useState('');
@@ -18,6 +17,32 @@ export default function ImageGallery({ search }) {
   const [per_page] = useState(12);
 
   useEffect(() => {
+    const fetchImages = () => {
+      const KEY = '29559865-360b254a5abc6663dbbd46c59';
+
+      const URL = `https://pixabay.com/api/?q=${search}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=${per_page}`;
+
+      axios
+        .get(URL)
+        .then(response => {
+          const data = response.data;
+          if (data.totalHits === 0) {
+            toast.error(`There are no images with "${search}" tags.`);
+            setStatus('idle');
+            return data;
+          } else {
+            setData(prevData => prevData.concat(data.hits));
+            setTotalHits(data.totalHits);
+            setStatus('resolved');
+          }
+        })
+        .catch(err => {
+          console.log(err);
+          setError(err.message);
+          setStatus('rejected');
+          toast.error(`${error}`);
+        });
+    };
     if (prevSearch !== search) {
       setData([]);
       setPage(1);
@@ -28,87 +53,57 @@ export default function ImageGallery({ search }) {
       fetchImages();
     }
     setPrevSearch(search);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [search, page]);
-
-  const fetchImages = () => {
-    const KEY = '29559865-360b254a5abc6663dbbd46c59';
-
-    const URL = `https://pixabay.com/api/?q=${search}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=${per_page}`;
-
-    axios
-      .get(URL)
-      .then((response) => {
-        const data = response.data;
-        if (data.totalHits === 0) {
-          toast.error(`There are no images with "${search}" tags.`);
-          setStatus('idle');
-          return data;
-        } else {
-          setData((prevData) => prevData.concat(data.hits));
-          setTotalHits(data.totalHits);
-          setStatus('resolved');
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-        setError(err.message);
-        setStatus('rejected');
-        toast.error(`${error}`);
-      });
-  };
+  }, [search, page, prevSearch, per_page, error]);
 
   function loadMore() {
-    setStatus('pending')
-    setPage(page + 1)
+    setStatus('pending');
+    setPage(page + 1);
   }
 
   function isMorePages() {
-    return page*per_page < totalHits
+    return page * per_page < totalHits;
   }
 
-
   if (status === 'idle') {
-      return (
-        <Wrapper>
-          <h1>Describe the images you want to see!</h1>
-        </Wrapper>
-      );
-    }
+    return (
+      <Wrapper>
+        <h1>Describe the images you want to see!</h1>
+      </Wrapper>
+    );
+  }
 
-    if (status === 'rejected') {
-      toast.error(`${error.message}`);
-      return <ToastContainer autoClose={3000} />;
-    }
-  
-    if (data.length > 0) { 
-      return (
-        <Wrapper>
-          <ImageGalleryStyle>
-            {data.map((hit) => {
-              return <ImageGalleryItem key={hit.id} data={hit} />;
-            })}
-          </ImageGalleryStyle>
+  if (status === 'rejected') {
+    toast.error(`${error.message}`);
+    return <ToastContainer autoClose={3000} />;
+  }
 
-          {isMorePages() && status !== 'pending' ?
-            (<Button onLoadMore={loadMore} />) 
-            : null
-          }
+  if (data.length > 0) {
+    return (
+      <Wrapper>
+        <ImageGalleryStyle>
+          {data.map(hit => {
+            return <ImageGalleryItem key={hit.id} data={hit} />;
+          })}
+        </ImageGalleryStyle>
 
-          {!isMorePages() ?
-            (<p>No more images with your tags :c</p>)
-          : (status === "pending" ? (<Loader/>) : null)
-          }
+        {isMorePages() && status !== 'pending' ? (
+          <Button onLoadMore={loadMore} />
+        ) : null}
 
-        </Wrapper>
-      );
-    }
-    
-    if (status === 'pending') {
-      return <Loader />;
-    }
+        {!isMorePages() ? (
+          <p>No more images with your tags :c</p>
+        ) : status === 'pending' ? (
+          <Loader />
+        ) : null}
+      </Wrapper>
+    );
+  }
+
+  if (status === 'pending') {
+    return <Loader />;
+  }
 }
 
 ImageGallery.propTypes = {
-  search: PropTypes.string.isRequired
-}
+  search: PropTypes.string.isRequired,
+};
